258b72fee7756c91bafd7d61e251aa1a8edb79bc
Kirill Fedorov <cyrillkel@gmail.com>
2024-04-26 02:57:46 +0300

    created adaptive menu and part section-hiro

--- js/script.js
+++ js/script.js
@@ -17,3 +17,103 @@ window.addEventListener("DOMContentLoaded", function () {
     );
   });
 });
+
+// speed scroll links menu
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+// for (let anchor of anchors) {
+//   anchor.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const blockID = anchor.getAttribute("href");
+//     document.querySelector("" + blockID).scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
+//   });
+// }
+
+var links = document.querySelectorAll(".menu__list a");
+console.log(links);
+
+// Для каждой ссылки добавляем обработчик события клика
+links.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    // Отменяем стандартное действие ссылки
+    event.preventDefault();
+
+    // Получаем значение атрибута href ссылки
+    var targetId = link.getAttribute("href");
+
+    // Находим элемент, к которому нужно прокрутить страницу
+    var targetElement = document.querySelector(targetId);
+
+    // Вычисляем позицию элемента на странице с учетом отступа
+    var offset = targetElement.offsetTop - 140; // Вычитаем 140px
+
+    // Прокручиваем страницу к элементу с учетом отступа
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth", // Добавляем плавность прокрутки
+    });
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var links = document.querySelectorAll(".nav__link a");
+//   links.forEach(function (link) {
+//     link.addEventListener("click", function (event) {
+//       event.preventDefault();
+//       var targetId = link.getAttribute("href");
+//       var targetElement = document.querySelector(targetId);
+//       var offset = targetElement.offsetTop - 100;
+
+//       // Добавляем небольшую задержку перед прокруткой
+//       setTimeout(function () {
+//         window.scrollTo({
+//           top: offset,
+//           behavior: "smooth",
+//         });
+//       }, 100); // Задержка в миллисекундах (100 мс)
+//     });
+//   });
+// });
+
+// mobile menu
+
+const menu = document.querySelector(".menu__body");
+const menuBtn = document.querySelector(".menu__icon");
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+    body.classList.toggle("lock");
+  });
+}
+
+const menuLinks = document.querySelectorAll(".menu__body a");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Закрываем меню
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("lock");
+  });
+});
+
+// =====================
+
+// anchors.forEach((anchor) => {
+//   anchor.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     const blockId = anchor.getAttribute("href").substring(1);
+//     document.getElementById(blockId).scrollIntoView({
+//       behavior: "smooth", // заменено smoth на smooth
+//       block: "start",
+//     });
+//   });
+// });
